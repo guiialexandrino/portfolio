38f2fbb953fb39d66dbee0f012e1bddebcde6bc6
Guilherme <guiialexandrino@gmail.com>
2023-02-23 16:06:57 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header />
+  <div class="__routeContainer">
+    <div class="__routeContent">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header/Header.vue';
+</script>
+
+<style lang="less" src="./assets/style/App.less" />
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="nav">
+      <span>NAV AQUI</span>
+      <span>NAV AQUI2</span>
+    </div>
+  </header>
+  <div class="logo-container">
+    <div class="logo-info">Logo</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less" src="./Header.less" />
