984739a6da060ca3652550628442c1f54258e0af
Guilherme <guiialexandrino@gmail.com>
2023-02-23 22:00:29 -0300

    Portfolio 1 commit

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,15 +1,33 @@
 <template>
   <header>
     <div class="nav">
-      <span>NAV AQUI</span>
-      <span>NAV AQUI2</span>
+      <div class="logo-header"></div>
+      <div>NAV AQUI2</div>
     </div>
   </header>
   <div class="logo-container">
-    <div class="logo-info">Logo</div>
+    <div class="logo-info">
+      <div class="avatar">
+        <img id="avatar-border" src="../../assets/images/avatar.png" />
+      </div>
+      <div class="color-logo"></div>
+      <div class="background-logo"></div>
+      <div class="span-frontend">
+        <h1>FRONTEND</h1>
+      </div>
+      <div class="span-developer">
+        <h1>DEVELOPER</h1>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const header = ref(null);
+
+onMounted(() => {});
+</script>
 
 <style scoped lang="less" src="./Header.less" />
