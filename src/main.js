c47b243a74afd4efcc20ae231878664017807354
Guilherme <guiialexandrino@gmail.com>
2023-02-24 21:53:05 -0300

    Vuex added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import PhosphorVue from 'phosphor-vue';
 import Button from './components/__Global/Button/Button.vue';
 
@@ -8,5 +9,6 @@ const app = createApp(App);
 
 app.component('Button', Button);
 app.use(router);
+app.use(store);
 app.use(PhosphorVue);
 app.mount('#app');
