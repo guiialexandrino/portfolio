ff8cd1337aee5b2f2c6958710fa4d86bd717f553
Guilherme <guiialexandrino@gmail.com>
2023-02-26 11:24:37 -0300

    Fix project template

--- src/views/Projects/ProjectTemplate.vue
+++ src/views/Projects/ProjectTemplate.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <h1>{{ route.params.name }}</h1>
+    <h1>{{ project.project }}</h1>
+    {{ project }}
   </div>
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
+import projects from './Projects.json';
+import { useRoute } from 'vue-router';
+import { onMounted, onUpdated } from 'vue';
+
 const route = useRoute();
+
+const project = projects.find((item) => item.project === route.params.name);
 </script>
 
 <style lang="less" src="./ProjectTemplate.less" />
