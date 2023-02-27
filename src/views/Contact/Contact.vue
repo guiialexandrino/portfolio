92e6e2efd91ff1fcba5f08ed259c69eab1ef4855
Guilherme <guiialexandrino@gmail.com>
2023-02-27 12:40:19 -0300

    Contacts done

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -1,8 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an CONTACT page</h1>
-    <h1>Catch a Wave</h1>
+  <div>
+    <div class="title">
+      <h2>contato_</h2>
+    </div>
+    <div class="contact-content">
+      <div>
+        <p>Você pode entrar em contato comigo pelo email:</p>
+        <h2>guilhermealexandrino.dev@gmail.com</h2>
+        <br />
+        <p>Ou pode me encontrar em:</p>
+        <div class="socialMedia">
+          <div
+            v-for="(item, index) in socialMedia"
+            :key="index"
+            :style="`background-image: url('${getImages(item.img)}')`"
+            @click="openLink(item.href)"
+          ></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<script setup>
+const socialMedia = [
+  {
+    name: 'Linkedin',
+    img: 'linkedin.svg',
+    href: 'https://www.linkedin.com/in/guilherme-alexandrino-149119247',
+  },
+  {
+    name: 'Github',
+    img: 'github.svg',
+    href: 'https://github.com/guiialexandrino',
+  },
+  {
+    name: 'Instagram',
+    img: 'instagram.svg',
+    href: 'https://www.instagram.com/guii.alexandrino',
+  },
+];
+
+function getImages(image) {
+  return new URL(`../../assets/icons/${image}`, import.meta.url);
+}
+
+function openLink(href) {
+  window.open(href, '_blank');
+}
+</script>
+
+<style scoped lang="less" src="./Contacts.less"></style>
