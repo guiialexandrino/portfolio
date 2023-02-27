1ded3fa256557daa2dde068114cd34b7d1252c35
Guilherme <guiialexandrino@gmail.com>
2023-02-27 18:20:28 -0300

    About done and Fixes

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,8 +1,70 @@
 <template>
-  <div class="about">
-    <h1>This is an ABOUT page</h1>
-    <h1>Catch a Wave</h1>
+  <div>
+    <div class="title">
+      <h2>sobre mim_</h2>
+    </div>
+    <div class="about-content">
+      <div class="max-content">
+        <h1>trajetória_</h1>
+        <p>
+          Olá :) sou aluno do
+          <b>
+            último semestre do curso de Sistemas de Informação pela Universidade
+            Federal de Santa Catarina.
+          </b>
+          Entretanto, antes de ir para a área da tecnologia me aventurei por
+          outras áreas como: moda, na qual sou formado em Design de Moda pela
+          UDESC (2014); design gráfico, atuei como estagiário desenvolvendo
+          material gráfico para distribuição em redes sociais e também atuei
+          como fotógrafo de moda e eventos.
+        </p>
+        <p>
+          No final de 2019, ingressei na área da tecnologia como
+          <b
+            >estagiário de frontend no Laboratório de Tecnologia de Informação e
+            Comunicação da Udesc.</b
+          >
+          No laboratório permaneci como estagiário durante 1 ano e 6 meses.
+          Após, esse período fiquei como responsável pelo frontend dentro do
+          laboratório, além de exercer a função de treinamento de novos
+          estagiários até o começo de 2023. Tive a oportunidade de trabalhar com
+          backend e um pouco de devOps. Entretanto,
+          <b>tenho bem mais familiaridade com o frontend</b>, principalmente com
+          o <b>framework VueJS</b> o qual tenho experiência há mais de 3 anos.
+        </p>
+        <h1 class="mt">experiência com_</h1>
+        <div class="list">
+          <div class="listContent">
+            <ul>
+              <li>Javascript</li>
+              <li>Typescript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Less</li>
+              <li>VueJS 2</li>
+              <li>VueJS 3</li>
+              <li>Vuetify</li>
+              <li>V-Router</li>
+              <li>Vuex</li>
+              <li>Consumo de APIs</li>
+              <li>Sockets</li>
+              <li>Nodejs</li>
+              <li>MongoDb</li>
+              <li>Git</li>
+              <li>Docker</li>
+              <li>DockerCompose</li>
+              <li>AWS - EC2</li>
+              <li>Máquinas Virtuais</li>
+              <li>Ambiente Linux</li>
+              <li>Ambiente Windows</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<script setup></script>
+
+<style scoped lang="less" src="./About.less"></style>
